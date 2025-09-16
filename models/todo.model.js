946084ef6d68e05b0ef4b8cb7021902b98adbdec
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-16 09:32:23 +0200

    Create Mongoose Schema

---
+++ models/todo.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const todoSchema = Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false,
+        required: true
+    }
+})
+
+export const Todo = mongoose.model('Todo', todoSchema);
