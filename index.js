2a025a6f89ba120b8190b4bbe4d280bc3244398e
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-16 09:37:45 +0200

    Add todo in DB successful

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import "dotenv/config"
 import express from "express"
 import morgan from "morgan"
+
 import { connectDB } from "./lib/db.js";
+
+import { Todo } from "./models/todo.model.js";
 
 const app = express();
 
 connectDB();
 
 morgan('tiny');
+
+const newTodo = new Todo ({
+    title: "Apprendre Javascript",
+})
+
+newTodo.save()
+    .then((todo) => {
+        console.log("Notre nouvelle todo ", todo)
+    })
+    .catch((error) => {
+        console.error(error.message)
+    })
 
 app.get('/', (req, res) => {
     res.send('Hello World')
