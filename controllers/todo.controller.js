31a78be651bd7880796798a30ad8bc6bf1ec9a34
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-16 09:43:00 +0200

    Prepare next service and controller for todos

---
+++ controllers/todo.controller.js
@@ -0,0 +1,13 @@
+const todoController = {
+    getTodos: (req, res) => {
+
+    },
+
+    addTodo: (req, res) => {
+
+    },
+
+    deleteTodo: (req, res) => {
+        
+    }
+}
---
+++ services/todo.service.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const todoService = {
+    getTodos: () => {
+
+    },
+
+    addTodo: () => {
+
+    },
+
+    deleteTodo: () => {
+        
+    }
+}
