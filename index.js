e3566fe92771c298dde392aa2527d917988ff193
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-19 06:34:40 +0200

    Add cors library

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import "express-async-errors"
 import express from "express"
 import morgan from "morgan"
+import cors from "cors"
 
 import { connectDB } from "./lib/db.js";
 import {todoController} from "./controllers/todo.controller.js";
@@ -12,10 +13,11 @@ connectDB();
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
+app.use('/todo', todoController.addTodo);
 app.use('/todo/:id', todoController.deleteTodo);
 app.use('/todo', todoController.getTodos);
-app.use('/todo', todoController.addTodo);
 
 morgan('tiny');
 
